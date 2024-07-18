96e1cc6bbf9d509562c2a4e57053dfce746d2d41
mgdb <1361520715@qq.com>
2024-07-18 15:33:52 +0800

    add qrealtion

--- src/controllers/basic_controller.py
+++ src/controllers/basic_controller.py
@@ -90,6 +90,7 @@ class BasicMAC:
         if self.args.obs_agent_id:
             inputs.append(th.eye(self.n_agents, device=batch.device).unsqueeze(0).expand(bs, -1, -1))
 
+        # actions_onehot = 6+n_enemy
         inputs = th.cat([x.reshape(bs, self.n_agents, -1) for x in inputs], dim=-1)
         return inputs
 
--- src/modules/agents/qrelation_rnn_agent.py
+++ src/modules/agents/qrelation_rnn_agent.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from .qrelation_rnn_gnn import adjacency_and_create_graph, GraphPyG, GCN
@@ -18,7 +19,10 @@ class QrelationRNNAgent(nn.Module):
             self.graph = GCN(args.rnn_hidden_dim, args.rnn_hidden_dim, args.n_actions)
 
         self.n_agents = args.n_agents
-        self.distance_index = []
+
+        self.dim_x = int((input_shape-6)/self.n_agents)
+        self.agent_feats_dim = self.dim_x*self.n_agents*2
+        self.distance_index = [5+self.dim_x*i for i in range(self.n_agents*2-1)]
 
     def init_hidden(self):
         # make hidden states on same device as model
@@ -26,15 +30,15 @@ class QrelationRNNAgent(nn.Module):
 
     def forward(self, inputs, hidden_state=None):
         b, a, e = inputs.size()
-        g = adjacency_and_create_graph(inputs, self.args.n_agents, dis_idx, self.graph_library)
+
+        # inputs.shape = [5, 96] 4+ x*(n_agent-1) + x*(n_agent) + (x-4) + 6+n_agent + n_agent
+        g = adjacency_and_create_graph(inputs[: self.agent_feats_dim], self.args.n_agents*2, self.distance_index, self.graph_library)
 
         x = F.relu(self.fc1(inputs.view(-1, e)), inplace=True)
         if hidden_state is not None:
             hidden_state = hidden_state.reshape(-1, self.args.rnn_hidden_dim)
         h = self.rnn(x, hidden_state)
         o = self.fc2(h)
-
-        # 4 + (n_agent-1)*(4+m) + (n_agent)*(4+m) + m
 
         q = self.graph(g, o)
         return q.view(b, a, -1), h.view(b, a, -1)
