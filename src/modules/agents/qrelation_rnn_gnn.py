eca39c24f93c1187d7d370d26d9868d2554d4faf
mgdb <1361520715@qq.com>
2024-07-24 15:17:08 +0800

    fix

--- src/modules/agents/qrelation_rnn_gnn.py
+++ src/modules/agents/qrelation_rnn_gnn.py
@@ -19,7 +19,8 @@ except:
 def adjacency_and_create_graph(ob, n_agents, dis_idx, graph_library):
     agent_ids = torch.arange(0, n_agents-1).to(ob.device)
     agent_ids = agent_ids.repeat(ob.shape[0], 1)
-    dis_l = torch.tensor(ob[:, dis_idx], device="cpu")
+    dis_l = ob[:, dis_idx]
+    dis_l = dis_l.clone().detach().cpu()
 
     agent_ids_new = torch.where(dis_l > 0, agent_ids, torch.zeros_like(agent_ids)-1).to(ob.device)
     # empty_array = torch.zeros_like(agent_ids_new).to(ob.device)
