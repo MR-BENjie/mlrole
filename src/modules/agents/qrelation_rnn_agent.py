a467922a791fce508f5fe9187a6b79536a509abe
mgdb <1361520715@qq.com>
2024-07-24 15:06:52 +0800

    fix

--- src/modules/agents/qrelation_rnn_agent.py
+++ src/modules/agents/qrelation_rnn_agent.py
@@ -31,6 +31,7 @@ class QrelationRNNAgent(nn.Module):
     def forward(self, inputs, hidden_state=None):
         b, a, e = inputs.size()
 
+        print(inputs.shape)
         # inputs.shape = [5, 96] 4+ x*(n_agent-1) + x*(n_agent) + (x-4) + 6+n_agent + n_agent
         g = adjacency_and_create_graph(inputs[: self.agent_feats_dim], self.args.n_agents*2, self.distance_index, self.graph_library)
 
